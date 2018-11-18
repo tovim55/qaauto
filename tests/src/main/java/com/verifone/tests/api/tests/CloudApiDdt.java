1a4133d42ede4c8ac538c73c220898b3ba9fc756
YegorP1 <YegorP1@verifone.com>
2018-11-18 14:32:44 +0200

    Cloud API_EmployeeDDT + email unique generation + All TCs

--- tests/src/main/java/com/verifone/tests/api/tests/CloudApiDdt.java
+++ tests/src/main/java/com/verifone/tests/api/tests/CloudApiDdt.java
@@ -4,6 +4,7 @@ import com.aventstack.extentreports.ExtentTest;
 import com.verifone.tests.BaseTest;
 import com.verifone.utils.DataDrivenUtils;
 import com.verifone.utils.apiClient.DataDrivenApi;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
@@ -21,13 +22,11 @@ public class CloudApiDdt extends BaseTest {
         return arrayObject;
     }
 
-
     @DataProvider(name = "employee")
     public Object[][] employee() throws Exception {
         Object[][] arrayObject = DataDrivenUtils.getExcelData(dataFile, "employee");
         return arrayObject;
     }
-
 
     @Test(dataProvider = "shift")
     public void cloudApiShiftDDT(String accessToken, String accGrantType, String accSSOURL, String uri, String requestMethod,
@@ -50,13 +49,14 @@ public class CloudApiDdt extends BaseTest {
         String email = uuid.replace("-", "") + "@getnada.com";
         if (requestMethod.equals("post")) {
             headers = "{RequestID:" + uuid + "}";
-            body = body.replace("test@getnada.com", email);
+           if(body!= null)
+           body = body.replace("test@getnada.com", email);
         }
         DataDrivenApi api = new DataDrivenApi((ExtentTest) test.get());
         api.startProsess(accessToken, accGrantType, accSSOURL, uri, requestMethod, headers, headersForGetToken, body,
                 expectedStatusCode, expectedResult, verifyList);
 
-    }
+   }
 
 
 }
