ea9befa1e9ed9527b0e16a77e3bdcdb5874db2dc
YegorP1 <YegorP1@verifone.com>
2019-03-24 14:24:45 +0200

    MarketPlace Automation tests: * Verify Apps subscribed * Subscribe an app * Unsubscribe an app * Login to VHQ

---
+++ tests/src/main/java/com/verifone/pages/marketPlacePages/VHQTestLogin.java
@@ -0,0 +1,36 @@
+package com.verifone.pages.marketPlacePages;
+
+import com.verifone.pages.BasePage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+public class VHQTestLogin extends BasePage {
+    private final static String url = "https://vhqtest.verifone.com/";
+    private final static String title = "Login with Verifone Identity Server" ;
+
+    public VHQTestLogin()
+    {
+        super(url, title);
+        navigate();
+    }
+
+    private By email = By.cssSelector("input[class='form-control input-lg']");
+    private By LoginVhqBtn = By.cssSelector("button[class='btn btn-login btn-block btn-lg'");
+    private By veriIframe = By.id("veriPassFrame");
+    private By passwordField = By.id("ipassword");
+    private By LoginBtn = By.id("btnPrimaryLogin");
+
+    public void LoginInVhq ()
+    {
+        sendKeys(email, "vfiappdirect@getnada.com");
+        ExpectedConditions.elementToBeClickable(LoginVhqBtn);
+        hoverAndClickOnElement(LoginVhqBtn);
+        //click(LoginVhqBtn);
+        ExpectedConditions.titleIs(title);
+        ExpectedConditions.frameToBeAvailableAndSwitchToIt(veriIframe);
+        switchToIframe(veriIframe);
+        sendKeys(passwordField, "Veri1234");
+        driver.switchTo().defaultContent();
+        click(LoginBtn);
+    }
+}
